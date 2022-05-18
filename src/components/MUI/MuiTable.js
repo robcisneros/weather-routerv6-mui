64846cfad7c631acce9d1438b4c8e88aefbd64a2
robcisneros <robertocisneroslp@gmail.com>
2022-05-18 01:37:42 -0500

    getAllDataWeather donegit add .

--- src/components/MUI/MuiTable.js
+++ src/components/MUI/MuiTable.js
@@ -1,5 +1,8 @@
 import classes from "./MuiTable.module.css";
 import { AiFillEdit, AiFillDelete } from "react-icons/ai";
+import useHttp from "../../hooks/use-http";
+import { useEffect, useState } from "react";
+
 import {
   TableContainer,
   Table,
@@ -11,57 +14,93 @@ import {
 } from "@mui/material";
 
 const MuiTable = () => {
-  const tableData = [
-    {
-      id: 1,
-      municipio: "Madero",
-      description: "descrip",
-      temperatura: 35,
-      acciones: "acciones",
-    },
-    {
-      id: 2,
-      municipio: "Jaumave",
-      description: "descrip",
-      temperatura: 35,
-      acciones: "acciones",
-    },
-    {
-      id: 3,
-      municipio: "Tampico",
-      description: "descrip",
-      temperatura: 35,
-      acciones: "acciones",
-    },
-    {
-      id: 4,
-      municipio: "Reynosa",
-      description: "descrip",
-      temperatura: 35,
-      acciones: "acciones",
-    },
-    {
-      id: 5,
-      municipio: "Burgos",
-      description: "descrip",
-      temperatura: 35,
-      acciones: "acciones",
-    },
-    {
-      id: 6,
-      municipio: "Gonzalez",
-      description: "descrip",
-      temperatura: 35,
-      acciones: "acciones",
-    },
-    {
-      id: 7,
-      municipio: "Matamoros",
-      description: "descrip",
-      temperatura: 35,
-      acciones: "acciones",
-    },
-  ];
+  // const tableData = [
+  //   {
+  //     id: 1,
+  //     municipio: "Madero",
+  //     description: "descrip",
+  //     temperatura: 35,
+  //     acciones: "acciones",
+  //   },
+  //   {
+  //     id: 2,
+  //     municipio: "Jaumave",
+  //     description: "descrip",
+  //     temperatura: 35,
+  //     acciones: "acciones",
+  //   },
+  //   {
+  //     id: 3,
+  //     municipio: "Tampico",
+  //     description: "descrip",
+  //     temperatura: 35,
+  //     acciones: "acciones",
+  //   },
+  //   {
+  //     id: 4,
+  //     municipio: "Reynosa",
+  //     description: "descrip",
+  //     temperatura: 35,
+  //     acciones: "acciones",
+  //   },
+  //   {
+  //     id: 5,
+  //     municipio: "Burgos",
+  //     description: "descrip",
+  //     temperatura: 35,
+  //     acciones: "acciones",
+  //   },
+  //   {
+  //     id: 6,
+  //     municipio: "Gonzalez",
+  //     description: "descrip",
+  //     temperatura: 35,
+  //     acciones: "acciones",
+  //   },
+  //   {
+  //     id: 7,
+  //     municipio: "Matamoros",
+  //     description: "descrip",
+  //     temperatura: 35,
+  //     acciones: "acciones",
+  //   },
+  // ];
+
+  const { sendRequest: fetchAPI } = useHttp();
+  const [dataWeather, setDataWeather] = useState([]);
+
+  useEffect(() => {
+    const url =
+      "https://react-http-5cc8c-default-rtdb.firebaseio.com/weatherBmore.json/";
+    const transformData = (dataObj) => {
+      const loadedData = [];
+
+      
+      for (const key in dataObj) {
+        
+        loadedData.push({
+          id: key,
+          estado: dataObj[key].estado,
+          municipio: dataObj[key].municipio,
+          description: dataObj[key].descripcion,
+          temperatura: dataObj[key].temperatura,
+        });
+
+        
+        
+      }
+
+      setDataWeather(loadedData);
+      
+    };
+
+    fetchAPI(
+      {
+        url: url,
+      },
+      transformData
+    );
+  }, [fetchAPI]);
 
   return (
     <TableContainer component={Paper} sx={{ maxHeight: "500px" }}>
@@ -76,7 +115,7 @@ const MuiTable = () => {
           </TableRow>
         </TableHead>
         <TableBody>
-          {tableData.map((row) => (
+          {dataWeather.map((row) => (
             <TableRow
               key={row.id}
               sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
